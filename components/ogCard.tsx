56b84af7961f29fe1fa11bc75086dc09babbcb72
Flavien Bonvin <flavien@coteries.com>
2022-12-05 20:12:06 +0100

    Test mega image cache

--- components/ogCard.tsx
+++ components/ogCard.tsx
@@ -11,7 +11,7 @@ const OGCard = ({ title, subtitle }: Props) => {
         background:
           "linear-gradient(133deg, rgba(255,107,107,1) 0%, rgba(255,230,109,1) 35%, rgba(78,205,196,1) 100%)",
       }}>
-      <div tw="relative flex h-[590px] w-[1160px] flex-col gap-10 bg-white p-10 px-24">
+      <div tw="relative flex h-[590px] w-[1160px] flex-col gap-10 bg-gray-300 p-10 px-24">
         <h1
           tw={`mt-6 max-w-[90%] text-7xl font-bold tracking-wide text-[#292F36]`}>
           {title}
